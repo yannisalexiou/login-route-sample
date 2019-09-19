a8270d20ff7edb2f3d303e32890b61fee728437a
yannisalexiou <johnmast7@yahoo.com>
2019-09-19 22:32:05 +0300

    added cloud db

--- config/database.js
+++ config/database.js
@@ -1,6 +1,5 @@
 // config/database.js
 module.exports = {
-
-	'url' : "mongodb://localhost:27017/LoginRouteDB"
-
+	url: 'mongodb://admin:admin123456@ds161397.mlab.com:61397/login-route'
+	//'url' : "mongodb://localhost:27017/LoginRouteDB"
 };
